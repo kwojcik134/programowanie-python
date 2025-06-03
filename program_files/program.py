f7386773c5cc05c807a12b9f20f78d30568384fe
Katarzyna Wojcik <katarzynawojcik76@gmail.com>
2025-06-03 14:56:14 +0200

    More code tweaks

--- program_files/program.py
+++ program_files/program.py
@@ -1,46 +1,33 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
-NUMERIC_VARIABLES = ['Time spent Alone', 'Social event attendance', 'Going outside', 'Friends circle size',
-                     'Post frequency']
+NUMERIC_VARIABLES = ['Time_spent_Alone', 'Social_event_attendance', 'Going_outside', 'Friends_circle_size',
+                     'Post_frequency']
+
+STRING_VARIABLES = ['Stage_fear', 'Drained_after_socializing']
 
 def preprocess(df):
-    # String columns -> {0, 1}
-    df['Stage fear'] = df['Stage fear'].map({'Yes': 1, 'No': 0})
-    df['Drained after socializing'] = df['Drained after socializing'].map({'Yes': 1, 'No': 0})
-    df['Personality'] = df['Personality'].map({'Extrovert': 1, 'Introvert': 0})
-
-    return df
-
-def load_and_clean(path):
-    #Load data into df
-    data = pd.read_csv(path)
-    df = data.copy()
-
-    #Get rid of missing values
-    df.dropna(inplace=True, ignore_index=True)
-
-    #Change column names - looks better for boxplot titles
-    mapper = {col: col.replace('_', ' ') for col in df.columns.values}
-    df.rename(mapper, axis=1, inplace=True)
-
-    print(df.head(5))
-    #Normalize numerical data
-    for col in NUMERIC_VARIABLES:
-        min_val = min(df[col])
-        max_val = max(df[col])
-        df[col] = (df[col] - min_val)/(max_val - min_val)
+    #Encoding yes/no data
+    # enc = LabelEncoder()
+    # to_encode = [col for col in STRING_VARIABLES]
+    # to_encode.append('Personality')
+    # for col in to_encode:
+    #     enc.fit_transform(df[col].tolist())
 
     print(df.head(5))
 
     return df
+
 
 def analyze(df):
     sns.set_theme(style = 'ticks', palette = 'cividis')
 
-    #
+    #General data shape
+
 
     #Distribution of target variable
     plt.figure()
@@ -56,7 +43,7 @@ def analyze(df):
     for i, num in enumerate(NUMERIC_VARIABLES, 1):
         plt.subplot(3, 2, i)
         sns.boxplot(x = 'Personality', y = num, data = df, hue = 'Personality', legend = False)
-        plt.title(f'{num} by Personality')
+        plt.title(f'{num.replace("_", " ")} by Personality')
         plt.ylabel('')
     plt.tight_layout()
     plt.show()
@@ -67,8 +54,14 @@ def analyze(df):
     sns.heatmap(df.corr(numeric_only = True), annot = True, cmap = 'cividis')
     plt.show()
 
-data = load_and_clean('C:/Users/RSM18/PycharmProjects/Pythom1/programowanie-python/data/personality_dataset.csv')
+path = 'C:/Users/RSM18/PycharmProjects/Pythom1/programowanie-python/data/personality_dataset.csv'
+# Load data into df
+raw_data = pd.read_csv(path)
+data = raw_data.copy()
 
-analyze(data)
+# Get rid of missing values
+data.dropna(inplace=True, ignore_index=True)
+
+preprocess(data)
 
 
