59d382165f355c09f1ae58b4f722c64a4bcb49ec
Katarzyna Wojcik <katarzynawojcik76@gmail.com>
2025-06-09 14:34:49 +0200

    Started working on model

--- program_files/program.py
+++ program_files/program.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 path = 'C:/Users/RSM18/PycharmProjects/Pythom1/programowanie-python/data/Healthcare-Diabetes.csv'
 # Load data into df
@@ -20,26 +21,26 @@ print(data.isnull().sum())
 x_set = data.drop('Outcome', axis = 1)
 y_set = data['Outcome']
 
-sns.set_theme(style = 'ticks', palette = 'deep')
-
-#Class distribution
-plt.figure()
-sns.countplot(x = y_set, data = data, hue = y_set, legend = False)
-plt.title('Class distribution of outcomes')
-plt.ylabel('Count')
-plt.xlabel('Outcome')
-plt.show()
-
-#Feature boxplots
-plt.figure(figsize = (10, 20))
-for i, col in enumerate(x_set.columns.tolist(), 1):
-    plt.subplot(3, 3, i)
-    sns.boxplot(data, x = y_set, y = col)
-    plt.title(f'{col} by outcome')
-plt.tight_layout()
-plt.ylabel('')
-plt.xlabel('')
-plt.show()
+# sns.set_theme(style = 'ticks', palette = 'deep')
+#
+# #Class distribution
+# plt.figure()
+# sns.countplot(x = y_set, data = data, hue = y_set, legend = False)
+# plt.title('Class distribution of outcomes')
+# plt.ylabel('Count')
+# plt.xlabel('Outcome')
+# plt.show()
+#
+# #Feature boxplots
+# plt.figure(figsize = (10, 20))
+# for i, col in enumerate(x_set.columns.tolist(), 1):
+#     plt.subplot(3, 3, i)
+#     sns.boxplot(data, x = y_set, y = col)
+#     plt.title(f'{col} by outcome')
+# plt.tight_layout()
+# plt.ylabel('')
+# plt.xlabel('')
+# plt.show()
 
 #Preprocessing
 x_train, x_test, y_train, y_test = train_test_split(x_set, y_set, test_size = 0.2, random_state = 42)
@@ -50,6 +51,16 @@ x_test_scaled = scaler.transform(x_test)
 
 #Linear SVC model
 lin_svc = LinearSVC()
+
+lin_svc.fit(x_train_scaled, y_train)
+y_lsvc_predicted = lin_svc.predict(x_test_scaled)
+
+lsvc_accuracy = accuracy_score(y_test, y_lsvc_predicted)
+print(lsvc_accuracy)
+lsvc_confusion = confusion_matrix(y_test, y_lsvc_predicted)
+print(lsvc_confusion)
+lsvc_rep = classification_report(y_test, y_lsvc_predicted)
+print(lsvc_rep)
 
 
 
